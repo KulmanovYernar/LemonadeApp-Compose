3966b4875bb067a048888c5b1caeed3de7a18284
yerakulmanov <200103127@stu.sdu.edu.kz>
2023-06-14 02:56:25 +0600

    Implement LemonTextAndImage fun

--- app/src/main/java/com/example/lemonadeapp_compose/MainActivity.kt
+++ app/src/main/java/com/example/lemonadeapp_compose/MainActivity.kt
@@ -1,15 +1,34 @@
 package com.example.lemonadeapp_compose
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lemonadeapp_compose.ui.theme.LemonadeAppComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +36,73 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LemonadeAppComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                LemonadeApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun LemonadeApp(modifier: Modifier = Modifier) {
+    var currentStep by remember {
+        mutableStateOf(1)
+    }
+    // A surface container using the 'background' color from the theme
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        val imageResource = when (currentStep) {
+            1 -> {
+                LemonTextAndImage(
+                    description = R.string.lemon_tree_desc,
+                    image = R.drawable.lemon_tree
+                )
+            }
+
+            2 -> LemonTextAndImage(
+                description = R.string.lemon_desc,
+                image = R.drawable.lemon_squeeze
+            )
+
+            3 -> LemonTextAndImage(
+                description = R.string.glass_of_lemonade_desc,
+                image = R.drawable.lemon_drink
+            )
+
+            4 -> LemonTextAndImage(
+                description = R.string.empty_glass_desc,
+                image = R.drawable.lemon_restart
+            )
+
+            else -> {}
+        }
+    }
+}
+
+@Composable
+fun LemonTextAndImage(description: Int, image: Int, modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+        Text(stringResource(id = description))
+        Spacer(modifier = modifier.height(32.dp))
+        Image(
+            painter = painterResource(id = image),
+            contentDescription = stringResource(id = description),
+            modifier = Modifier
+                .wrapContentSize()
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun LemonadePreview() {
     LemonadeAppComposeTheme {
-        Greeting("Android")
+        LemonadeApp()
     }
 }
